b7a90348a5c92d5a6aa3ead1ee4756f2dcfa05e9
jceee5510 <josephchoi@Josephs-MacBook-Pro.local>
2017-02-21 11:58:59 -0800

    ww
    w

---
+++ Demo.java
@@ -0,0 +1,33 @@
+
+/**
+ * This will be our demo. Each student should add their own method to the code
+ */
+public class Demo
+{
+    // instance variables
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class Demo
+     */
+    public Demo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Getter for instance variable x.
+     *
+     */
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+}
